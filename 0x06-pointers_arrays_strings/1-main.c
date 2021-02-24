74c7a99e7e82c618630d5d2c6b5ab22a030b5599
Yosef-S-A <yosefsamuel22@gmail.com>
2021-02-24 17:28:25 +0000

    tasks

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char s1[98] = "Hello ";
@@ -14,7 +14,11 @@ char *p;
 
 printf("%s\n", s1);
 printf("%s", s2);
-p = _strcat(s1, s2);
+p = _strncat(s1, s2, 1);
+printf("%s\n", s1);
+printf("%s", s2);
+printf("%s\n", p);
+p = _strncat(s1, s2, 1024);
 printf("%s", s1);
 printf("%s", s2);
 printf("%s", p);
