7d95bed8b10e00c36576ab4f8bd92458f4cec3fd
Yosef-S-A <yosefsamuel22@gmail.com>
2021-02-25 11:42:57 +0000

    tasks

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,19 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
-* File: holberton.h
-* Auth: Yosef Samuel
-* Desc: Header file containing prototypes for all functions
-*       used in the 0x05-pointers_arrays_strings directory.
-*/
-
-#include <stdlib.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -22,5 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
