9ec9a1d5eaac69bc6a0e47687250a788f6c05751
Yosef-S-A <yosefsamuel22@gmail.com>
2021-03-21 06:59:28 +0000

    task

---
+++ 0x12-singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,77 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node - function that adds a new node at the start of adrsList list
+ * @head: pointer to pointer to adrsList list
+ * @ptr: address to be added
+ * Return: the address of head, or NULL if it failed
+ **/
+adrsList *add_node(adrsList **head, void *ptr)
+{
+adrsList *new;
+
+new = malloc(sizeof(adrsList));
+if (new == NULL)
+{
+free_adrsList(*head);
+exit(98);
+}
+(*new).ptr = ptr;
+(*new).next = *head;
+*head = new;
+return (*head);
+}
+
+/**
+ * free_adrsList - frees newly craeted address list
+ * @h: head of the list
+ **/
+void free_adrsList(adrsList *h)
+{
+adrsList *frN;
+
+while (h)
+{
+frN = h;
+h = (*h).next;
+free(frN);
+}
+}
+
+/**
+ * print_listint_safe - prints listint_t list, safe version
+ * @head: pointer to elements of type listint_t
+ * Return: the number of nodes in the list
+ **/
+size_t print_listint_safe(const listint_t *head)
+{
+size_t counter;
+adrsList *newList, *newHead;
+
+counter = 0;
+if (head == NULL)
+return (counter);
+
+newHead = newList = NULL;
+while (head)
+{
+while (newList)
+{
+if ((*newList).ptr == head)
+{
+printf("-> [%p] %d\n", (void *)head, (*head).n);
+free_adrsList(newHead);
+return (counter);
+}
+newList = (*newList).next;
+}
+printf("[%p] %d\n", (void *)head, (*head).n);
+newList = add_node(&newHead, (void *)head);
+head = (*head).next;
+counter++;
+}
+free_adrsList(newHead);
+return (counter);
+}
---
+++ 0x12-singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - function that frees linked list
+ * @h: pointer to pointer to elements of type listint_t
+ * Return: the number of nodes in the list that were freed
+ **/
+size_t free_listint_safe(listint_t **h)
+{
+size_t counter;
+listint_t *temp;
+adrsList *newList, *newHead;
+
+counter = 0;
+if (*h == NULL)
+return (counter);
+newHead = newList = NULL;
+while (*h)
+{
+while (newList)
+{
+if ((*newList).ptr == *h)
+{
+free_adrsList(newHead);
+newHead = NULL;
+*h = NULL;
+return (counter);
+}
+newList = (*newList).next;
+}
+newList = add_node(&newHead, (void *)*h);
+temp = *h;
+*h = (*(*h)).next;
+free(temp);
+counter++;
+}
+free_adrsList(newHead);
+*h = NULL;
+return (counter);
+}
