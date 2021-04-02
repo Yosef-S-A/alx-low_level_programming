ff1e84ed4acb22f60350e5cf80ccd5ca0809eb17
Yosef-S-A <yosefsamuel22@gmail.com>
2021-04-02 17:20:56 +0000

    tasks

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,11 +1,29 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <elf.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *file1, char *file2);
+void read_elf_header(int32_t fd, Elf32_Ehdr *elf_header);
+bool is_ELF(Elf32_Ehdr eh);
+void print_os(Elf32_Ehdr eh);
+void print_typ(Elf32_Ehdr eh);
+void print_clas(Elf32_Ehdr eh);
+void print_dta(Elf32_Ehdr eh);
+void print_vrsn(Elf32_Ehdr eh);
+void print_elf_header(Elf32_Ehdr elf_header);
 
 #endif
